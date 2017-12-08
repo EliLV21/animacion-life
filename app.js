f096ef9ba4d1ec214955684254fb74feda0ccce9
Miriam Elizabeth Laguna Velazquez <miriam.elizabeth.lv@gmail.com>
2017-12-08 09:47:25 -0600

    animacion-life

---
+++ app.js
@@ -0,0 +1,4 @@
+$(document).ready(
+    $('.segunda-animacion').velocity({rotateY: '180deg', rotateX: '180deg', top: '-13px'}, {duration: '1000', easing: 'spring', loop: true, delay:100}),
+    $('.primera-animacion').velocity({ rotateY: '-180deg'}, { duration: '1000', easing: 'spring', loop: true, delay: 100 })
+);
