01b1662cac0e40e7a1b781c464b7889891654e5c
Miriam Elizabeth Laguna Velazquez <miriam.elizabeth.lv@gmail.com>
2017-12-08 13:45:44 -0600

    prueba 1-1

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 $(document).ready(
-    $('.segunda-animacion').velocity({ rotateY: '180deg', rotateX: '180deg', top: '-13px' }, { duration: '1000', easing: 'spring', loop: true, delay: 100 }),
-    $('.primera-animacion').velocity({ rotateY: '-180deg' }, { duration: '1000', easing: 'spring', loop: true, delay: 100 })
+    $('.segunda-animacion').velocity({ rotateY: '180deg', rotateX: '180deg', top: '-13px' }, { duration: '4000', easing: 'spring', loop: true, delay: 100 }),
+    $('.primera-animacion').velocity({ rotateY: '-180deg' }, { duration: '3000', easing: 'spring', loop: true, delay: 100 }),
+    $('.letra-l-svg').velocity({ rotateY: '-180deg', rotateX: '180deg'}, { duration: '1000', easing: 'spring', loop: true, delay: 100 }),
+    $('.cls-1').velocity({ fill: '#333e48'})
 );
+
+// function cargarPagina(){
+//     cambiarColor()
+// };
+// function cambiarColor(){
+
+// };
